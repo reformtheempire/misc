a0e1179de683099b67b4eb6b9bee5753d572cf95
creativemario <thomas@mather.ch>
2016-12-18 01:27:13 +0000

    Showhow: update SQLUtil for the last release.
    Set example values so we can build the app.

--- ht.tm.dev.currys.showhow/src/ht/tm/dev/currys/showhow/db/util/SQLUtil.java
+++ ht.tm.dev.currys.showhow/src/ht/tm/dev/currys/showhow/db/util/SQLUtil.java
@@ -1,12 +1,14 @@
 package ht.tm.dev.currys.showhow.db.util;
 
 public class SQLUtil {
-	
-	public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-	public static final String DB_URL = "DB_URL";
 
-	public static final String USER = "USER";
-	public static final String PASS = "PASS";
+	public static final String DB_HOSTNAME = "example";
+
+	public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	public static final String DB_URL = "jdbc:mysql://" + DB_HOSTNAME + ":3306/example";
+
+	public static final String USER = "example";
+	public static final String PASS = "example";
 
 	public static void loadDBDriver() throws ClassNotFoundException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
