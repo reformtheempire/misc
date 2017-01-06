b8bead9316745b1e1d53143bb3f39d94132ddc4d
creativemario <thomas@mather.ch>
2017-01-06 10:18:09 +0000

    ApplicationUpdater: Initial Commit
    Commit the application updater.

---
+++ ht.tm.dev.applicationupdater/src/ht/tm/dev/applicationupdater/db/SQLConnectionManager.java
@@ -0,0 +1,40 @@
+package ht.tm.dev.applicationupdater.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import ht.tm.dev.applicationupdater.db.util.SQLUtil;
+
+public class SQLConnectionManager {
+
+	public static int currentConnections;
+
+	public static Connection getConnectionToDB() throws SQLException {
+		Connection conn = null;
+		conn = DriverManager.getConnection(SQLUtil.DB_URL, SQLUtil.USER, SQLUtil.PASS);
+		currentConnections++;
+		if (conn == null) {
+			throw new SQLException("Cannot get connection");
+		}
+		return conn;
+	}
+
+	public static boolean closeConnection(Connection conn) {
+
+		try {
+			if (conn != null) {
+				if (!conn.isClosed()) {
+					conn.close();
+					currentConnections--;
+					return true;
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return false;
+	}
+
+}
